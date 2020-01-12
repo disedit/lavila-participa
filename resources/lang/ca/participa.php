9527da2f4d08183dcae2a31046778969c6756d96
Guillem <guillem@disedit.com>
2020-01-12 19:16:24 +0100

    Update language files

--- resources/lang/ca/participa.php
+++ resources/lang/ca/participa.php
@@ -11,10 +11,10 @@ return [
     'twitter_locale' => 'es',
     'facebook_locale' => 'ca_ES',
     'heading' => 'Tu decideixes en què inverteix l\'Ajuntament!',
-    'subheading' => 'El Govern de [city_name] t\'obri la possibilitat de participar activament en les decisions que més t\'afecten.',
-    'tweet' => 'He votat en què projectes vull que [city_name] invertisca',
-    'description' => 'Tu decideixes les inversions de l\'Ajuntament. El Govern de [city_name] t\'obri la possibilitat de participar activament en les decisions que més t\'afecten. Junts volem millorar [city_name], i el teu vot compta!',
-    'keywords' => '[city_name], ajuntament, ayuntamiento, govern, obert, participacio ciutadana, democracia participativa, [city_name] participa, votació, votación, referendum',
+    'subheading' => 'El Govern de la Vila Joiosa t\'obri la possibilitat de participar activament en les decisions que més t\'afecten.',
+    'tweet' => 'He votat en què projectes vull que @Aytolavila invertisca',
+    'description' => 'Tu decideixes les inversions de l\'Ajuntament. El Govern de la Vila Joiosa t\'obri la possibilitat de participar activament en les decisions que més t\'afecten. Junts volem millorar la Vila Joiosa, i el teu vot compta!',
+    'keywords' => 'la Vila Joiosa, ajuntament, ayuntamiento, govern, obert, participacio ciutadana, democracia participativa, la Vila Joiosa participa, votació, votación, referendum',
 
     /*
     |--------------------------------------------------------------------------
