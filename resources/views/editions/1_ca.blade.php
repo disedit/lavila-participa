c4e26a12d6bd434fe52e4842f524fdb89f213787
Guillem <guillem@disedit.com>
2020-01-12 19:42:10 +0100

    Update edition text

---
+++ resources/views/editions/1_ca.blade.php
@@ -0,0 +1,133 @@
+<section aria-labelledby="about-1" class="about-block" aria-live="polite">
+    <h3 id="about-1">La Vila Participa</h3>
+    <img src="/images/choice-2.svg" class="about-block__thumbnail" alt="" aria-hidden="true" />
+
+    <p>
+      L’<strong>Ajuntament de la Vila Joiosa</strong> ha establert la regeneració
+      democràtica com uns dels seus pilars bàsics. Una regeneració que passa per
+      donar veu als ciutadans i ciutadanes i apropar-los a la pressa de decisions.
+      Una participació que té per finalitat que la ciutania puga assolir majors quotes
+      de corresponsabilitat política perquè <strong>“co-governen” el seu ajuntament, el seu poble</strong>.
+    </p>
+    <p>
+      Ja vam començar fa dos anys amb aquest format de pressupostos participatius
+      i en aquesta segona edicio, el procés de participaciò serà molt més ampli,
+      dinàmic i fàcil.
+    </p>
+    <p>
+      <strong>La Vila Participa</strong> pretén aprofundir en la participació real i efectiva de
+      la ciutadania.
+    </p>
+    <p>
+      A tal efecte, l'Equip de Govern ha decidit que els <strong>10 projectes</strong> que se
+      sotmeten a votació hagen estat proposats per la ciutadania. Al mes de <strong>febrer
+      de 2020</strong>, s'obrirà un termini de quatre setmanes per tal que els ciutadans i
+      les ciutadanes presenten les seues propostes d'inversions. Aquestes propostes
+      seran avaluades i analitzades pel Tècnics Municipals i cada proposta tindrà
+      un pressupost estimat i una estimació de beneficiaris, així com el motiu pel
+      qual queden desestimades.
+    </p>
+    <p>
+      Posteriorment, seran els ciutadans i ciutadanes els qui es pronunciaran sobre
+      <strong>quins 3 projectes consideren prioritaris</strong>. La informació al voltant dels projectes
+      es podrà consultar a la web municipal, als llocs habilitats per a l’emissió del
+      parer de la ciutadania, així com als diferents mitjans de comunicació.
+      Es farà una presentació pública dels 10 projectes sotmesos a votació.
+    </p>
+    <p>
+      Els responsables dels projectes elegits per a ser votats tindran l’oportunitat
+      de presentar-los públicament en una sessió oberta a la ciutadania, sense
+      perjudici que la persona o grup que així ho considere puga fer més accions
+      de promoció de la seua proposta. 
+    </p>
+</section>
+
+<section id="calendar" aria-labelledby="about-2" class="about-block about-block--calendar">
+    <h3 id="about-2">Calendari</h3>
+
+    <div class="phases phases-compact">
+        <div class="phase">
+            <div class="phase__calendar">
+                <div class="calendar">
+                    <div class="calendar__heading">Propostes</div>
+                    <div class="calendar__dates">
+                        <div class="calendar__days"><strong>1-29</strong></div>
+                        <div class="calendar__month">febrer</div>
+                    </div>
+                </div>
+            </div>
+            <div class="phase__text">
+                <p>
+                    La ciutadania i associacions poden fer
+                    arribar les seues propostes emplenant el
+                    formulari online <em>(properament)</em>.
+                </p>
+            </div>
+        </div>
+
+        <div class="phase__path">
+            <img src="/images/path.svg" alt="" />
+        </div>
+
+        <div class="phase">
+            <div class="phase__calendar">
+                <div class="calendar">
+                    <div class="calendar__heading">Avaluació</div>
+                    <div class="calendar__dates">
+                        <div class="calendar__days"><strong>1</strong> - <strong>31</strong></div>
+                        <div class="calendar__month">març</div>
+                    </div>
+                </div>
+            </div>
+            <div class="phase__text">
+                <p>Informació tècnica dels projectes presentats per determinar-ne pressupost.</p>
+            </div>
+        </div>
+
+        <div class="phase__path mt-0">
+            <img src="/images/path-2.svg" alt="" style="transform: rotate(180deg);" />
+        </div>
+
+        <div class="phase">
+            <div class="phase__calendar">
+                <div class="calendar">
+                    <div class="calendar__heading">Campanya</div>
+                    <div class="calendar__dates">
+                        <div class="calendar__days"><strong>1</strong> - <strong>31</strong></div>
+                        <div class="calendar__month">abril</div>
+                    </div>
+                </div>
+            </div>
+            <div class="phase__text">
+                <p>
+                    S'exposaran i explicaran els 10 projectes que serán objecte de
+                    votació, període i llocs on votar en un acte públic.
+                </p>
+            </div>
+        </div>
+
+        <div class="phase__path">
+            <img src="/images/path.svg" alt="" />
+        </div>
+
+        <div class="phase">
+            <div class="phase__calendar">
+                <div class="calendar calendar--red">
+                    <div class="calendar__heading">Votació</div>
+                    <div class="calendar__dates">
+                        <div class="calendar__days"><strong>1</strong> - <strong>15</strong></div>
+                        <div class="calendar__month">maig</div>
+                    </div>
+                </div>
+            </div>
+            <div class="phase__text">
+                <p>
+                    Es podrà votar telemàticament per mitjà del sistema habilitat
+                    per internet o als punts habilitats per a la votació presencial.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <p class="about-block__fine-print mt-4 mb-0">* Les dates del calendari poden sofrir variacions</p>
+</section>
