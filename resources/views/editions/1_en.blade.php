5d8647bf756f5c34a412d588d44334ca0818bdcd
Guillem <guillem@disedit.com>
2020-02-25 18:27:40 +0100

    Add English translation

---
+++ resources/views/editions/1_en.blade.php
@@ -0,0 +1,149 @@
+<section aria-labelledby="about-1" class="about-block" aria-live="polite">
+    <h3 id="about-1">La Vila Participa</h3>
+    <img src="/images/choice-2.svg" class="about-block__thumbnail" alt="" aria-hidden="true" />
+
+    <p>
+      The <strong>Town Council of la Vila Joiosa</strong> believes in democratic regeneration
+      as one of its fundamental pillars. A regeneration that entails giving citizens
+      a voice and bringing them closer to the decision-making process. A participation
+      that aims to give the citizens a bigger share of political co-responsability,
+      so they can <strong>“co-govern” their council, their town.</strong>
+    </p>
+    <p>
+      It started two years ago with this format of participatory budgeting and for this second edition, the process will be more dynamic, broader in reach, and easier to take a part in.
+    </p>
+    <p>
+      <strong>La Vila Participa</strong> aims to delve into the real and effective participation of its citizens.
+    </p>
+    <p>
+      To this end, the Government of la Vila Joiosa has decided that the <strong>ten projects</strong>
+      that make it to the ballot will be chosen from among citizen submissions.
+      On <strong>February 2020, a four-week proposal phase will start so that citizens
+      may submit their proposals for local projects. The proposals will then be assessed and
+      analysed by municipal technicians, who will assign an estimated budget, determine
+      who would benefit from the project, or provide a technical reason should the proposal be rejected.
+    </p>
+    <p>
+      Afterwards, the citizens will choose up to <strong>three projects</strong>
+      that they prefer to be carried out. The information regarding the proposals
+      will be available to consult on this website, the polling stations, as well as
+      other different media. A public presentation event of the ten projects will
+      also be held.
+    </p>
+    <p>
+      The citizens or associations that have put forward a proposal chosen for the
+      final ballot will have the chance to present it publicly in an open session
+      organised by the Council, which will be in addition to any other form of
+      promotion that they may carry out on their own if they wish.
+    </p>
+</section>
+
+<section id="calendar" aria-labelledby="about-2" class="about-block about-block--calendar">
+    <h3 id="about-2">Calendar</h3>
+
+    <div class="phases phases-compact">
+        <div class="phase">
+            <div class="phase__calendar">
+                <div class="calendar">
+                    <div class="calendar__heading">Proposals</div>
+                    <div class="calendar__dates">
+                        <div class="calendar__days"><strong>1-29</strong></div>
+                        <div class="calendar__month">February</div>
+                    </div>
+                </div>
+            </div>
+            <div class="phase__text">
+                <p>
+                    Citizens and associations may submit their proposals by filling out the <a href="/propose">online form</a> or at the Town Council.
+                </p>
+            </div>
+        </div>
+
+        <div class="phase__path">
+            <img src="/images/path.svg" alt="" />
+        </div>
+
+        <div class="phase">
+            <div class="phase__calendar">
+                <div class="calendar">
+                    <div class="calendar__heading">Assessment</div>
+                    <div class="calendar__dates">
+                        <div class="calendar__days"><strong>1</strong> - <strong>31</strong></div>
+                        <div class="calendar__month">March</div>
+                    </div>
+                </div>
+            </div>
+            <div class="phase__text">
+                <p>The submitted projects will be assessed by technicians to determine their approximate cost.</p>
+            </div>
+        </div>
+
+        <div class="phase__path mt-0">
+            <img src="/images/path-2.svg" alt="" style="transform: rotate(180deg);" />
+        </div>
+
+        <div class="phase">
+            <div class="phase__calendar">
+                <div class="calendar">
+                    <div class="calendar__heading">Campaign</div>
+                    <div class="calendar__dates">
+                        <div class="calendar__days"><strong>1</strong> - <strong>31</strong></div>
+                        <div class="calendar__month">April</div>
+                    </div>
+                </div>
+            </div>
+            <div class="phase__text">
+                <p>
+                    The ten selected projects will be presented and exhibited in an open event organised by the Council.
+                </p>
+            </div>
+        </div>
+
+        <div class="phase__path">
+            <img src="/images/path.svg" alt="" />
+        </div>
+
+        <div class="phase">
+            <div class="phase__calendar">
+                <div class="calendar calendar--red">
+                    <div class="calendar__heading">Vote</div>
+                    <div class="calendar__dates">
+                        <div class="calendar__days"><strong>15</strong> - <strong>15</strong></div>
+                        <div class="calendar__month">Apr. / May.</div>
+                    </div>
+                </div>
+            </div>
+            <div class="phase__text">
+                <p>
+                  People will be able to vote online on this website or in person at the polling stations.
+                </p>
+            </div>
+        </div>
+
+
+        <div class="phase__path mt-0">
+            <img src="/images/path-2.svg" alt="" style="transform: rotate(180deg);" />
+        </div>
+
+        <div class="phase">
+            <div class="phase__calendar">
+                <div class="calendar">
+                    <div class="calendar__heading">Results</div>
+                    <div class="calendar__dates">
+                        <div class="calendar__days"><strong>18</strong></div>
+                        <div class="calendar__month">May</div>
+                    </div>
+                </div>
+            </div>
+            <div class="phase__text">
+                <p>
+                    Results will be published
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <p class="about-block__fine-print mt-4 mb-0">* Dates on the calendar may change</p>
+</section>
+
+
