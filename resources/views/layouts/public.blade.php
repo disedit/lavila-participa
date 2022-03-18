37589f77536528537a02e6fa5a91b27a01534d71
guillem <guillem@disedit.com>
2022-03-18 16:21:56 +0100

    Add og:image

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -7,7 +7,6 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="/images/favicon.png">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @if(Auth::user())
         <meta name="jwt-token" content="{{ Auth::user()->api_token }}">
